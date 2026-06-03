6a991f43452c5787ec0778184b690e3b1c912cee
Khushi78955 <khushithisside26@gmail.com>
2026-06-04 04:26:22 +0530

    implement room code synchronization

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const http = require("http");
 const {Server} = require("socket.io")
+const prisma = require("./config/db")
 
 const PORT = 3000;
 
@@ -13,12 +14,47 @@ const io = new Server(server, {
 
 io.on("connection", function(socket){
     console.log("User connected: ", socket.id)
-    socket.on("join-room", function(roomCode){
+
+    socket.on("join-room", async function(roomCode){
         console.log("Joining room:", roomCode)
         socket.join(roomCode)
+        
+        
+        const room = await prisma.room.findUnique({
+            where: {
+                roomCode
+            }
+        })
+        if(!room){
+            return;
+        }
+
+        const code = room.currentCode;
+        if(!code){
+            return;
+        }
+        
+        socket.emit("sync-code", code)
     })
-    socket.on("code-change", function(data){
+
+
+
+
+    socket.on("code-change", async function(data){
         const {roomCode, code} = data;
+        console.log("Saving code for room:", roomCode);
+
+        await prisma.room.update({
+            where: {
+                roomCode
+            },
+            data: {
+                currentCode: code
+            }
+        })
+
+
+
         socket.to(roomCode).emit(
             "code-change",
             code
