d65e88418a44463a14ea410ceca53533ea153e67
Khushi78955 <khushithisside26@gmail.com>
2026-06-04 01:46:31 +0530

    test socket room communication

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,32 @@
 const app = require("./app");
+const http = require("http");
+const {Server} = require("socket.io")
+
 const PORT = 3000;
-app.listen(PORT, function(){
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", function(socket){
+    console.log("User connected: ", socket.id)
+    socket.on("join-room", function(roomCode){
+        console.log("Joining room:", roomCode)
+        socket.join(roomCode)
+    })
+    socket.on("code-change", function(data){
+        const {roomCode, code} = data;
+        socket.to(roomCode).emit(
+            "code-change",
+            code
+        )
+    })
+})
+
+
+server.listen(PORT, function(){
     console.log(`Server is running on port ${PORT}`)
 })
