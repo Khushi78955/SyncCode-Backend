f6db6ca61ac19aecdd4c6cf382d79df28adefe45
Khushi78955 <khushithisside26@gmail.com>
2026-06-07 01:41:52 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
+
 const authRouter = require("./routes/auth.routes")
 const roomRouter = require("./routes/room.routes");
 const otpRouter = require("./routes/otp.routes")
@@ -7,9 +9,14 @@ const otpRouter = require("./routes/otp.routes")
 const errorMiddleware = require("./middleware/error.middleware");
 
 
+
 const app = express()
+
 app.use(express.json())
 app.use(helmet())
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 app.use("/api/auth", authRouter)
 app.use("/api/rooms", roomRouter)
@@ -18,3 +25,4 @@ app.use("/api/otp", otpRouter)
 app.use(errorMiddleware)
 
 module.exports = app;
+
