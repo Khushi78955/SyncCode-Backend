7b937b5d43d5eeae6a1c3dc06e6602ef4e381424
Khushi78955 <khushithisside26@gmail.com>
2026-06-09 15:22:28 +0530

    Remove frontend from backend repository

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return (
-        <footer>
-            <p>Built By Khushi</p>
-        </footer>
-    )
-}
-
-export default Footer
--- frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar(){
-    return (
-        <div>
-            <h1>SyncCode</h1>
-        </div>
-    )
-}
-
-export default Navbar;
